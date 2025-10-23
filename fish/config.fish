059ab8cb2c48f62ad992e9c163e72d0e0321a67a
Allen Hansrisuk <ahansrisuk@gmail.com>
2025-10-23 07:50:38 -0400

    fish config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.emacs.d/bin
+    fish_add_path /opt/nvim 
+    
+end
