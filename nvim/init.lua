2d41efa7ed5488a775e18151dee83a62ccb0f5fa
Allen Hansrisuk <ahansrisuk@gmail.com>
2025-10-31 16:01:39 -0400

    gruvbox

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd.colorscheme("catppuccin-mocha")
+-- vim.cmd.colorscheme("catppuccin-mocha")
+
+vim.o.background = "dark" 
+vim.cmd.colorscheme("gruvbox")
